cfdcafc19d02b39c63f5341b91e803b236bf8834
libill <770737185@qq.com>
2014-11-12 00:04:06 +0800

    add s

--- src/com/android/informationdemo/SleepChartActivity.java
+++ src/com/android/informationdemo/SleepChartActivity.java
@@ -4,13 +4,17 @@ import com.android.informationdemo.view.chart.SleepChartView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SleepChartActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
 		setContentView(new SleepChartView(this));
 	}
 }
--- src/com/android/informationdemo/view/chart/SleepChartView.java
+++ src/com/android/informationdemo/view/chart/SleepChartView.java
@@ -1,23 +1,79 @@
 package com.android.informationdemo.view.chart;
 
+import android.R.integer;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class SleepChartView extends View{
+public class SleepChartView extends View {
 
-	public SleepChartView(Context context){
-		super(context);
-	}
+	private Context mContext;
+	private Paint mPaint = new Paint();
+	private float width;
+	private float height;
 	
+	private int xSpaceNum = 10;
+	private int ySpaceNum = 10;
+	private float xSpace;
+	private float ySpace;
+
+	public SleepChartView(Context context) {
+		super(context);
+		this.mContext = context;
+	}
+
 	public SleepChartView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
+		
+		initData();
+
+		drawChart(canvas);
+
+	}
+	
+	public void initData(){
+		width = this.getWidth();
+		height = this.getHeight();
+		xSpace = width/xSpaceNum;
+		ySpace = height/ySpaceNum;
+	}
+	
+	public void drawChart(Canvas canvas){
+		// 创建画笔
+		mPaint.setColor(Color.RED);// 设置红色
+
+		mPaint.setAntiAlias(true);// 设置画笔的锯齿效果。 true是去除，大家一看效果就明白了
+
+		float startX = 0;
+		float startY = 0;
+		float stopX = width;
+		float stopY = 0;
+		
+		// X横坐标
+		for (int i = 0; i <= 10; i++) {
+			canvas.drawLine(startX, startY, stopX, stopY, mPaint);
+			startY += ySpace;
+			stopY += ySpace;
+		}
+		
+		// Y横坐标
+		startX = 0;
+		startY = 0;
+		stopX = 0;
+		stopY = height;
+		for (int i = 0; i <= 10; i++) {
+			canvas.drawLine(startX, startY, stopX, stopY, mPaint);
+			startX += xSpace;
+			stopX += xSpace;
+		}
 	}
 
 }
