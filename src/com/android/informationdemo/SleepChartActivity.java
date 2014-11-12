8f725053fdbf5d6216fbb477f4c4fa1473f4ba01
libill <770737185@qq.com>
2014-11-12 18:50:28 +0800

    add week

--- src/com/android/informationdemo/SleepChartActivity.java
+++ src/com/android/informationdemo/SleepChartActivity.java
@@ -1,20 +1,41 @@
 package com.android.informationdemo;
 
-import com.android.informationdemo.view.chart.SleepChartView;
-
+import android.R.integer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class SleepChartActivity extends Activity{
+import com.android.informationdemo.view.chart.SleepChartView;
 
+public class SleepChartActivity extends Activity{
+	int i=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         
-		setContentView(new SleepChartView(this));
+//		setContentView(new SleepChartView(this));
+        setContentView(R.layout.activity_sleep);
+        
+        final SleepChartView sleepChartView = (SleepChartView) findViewById(R.id.sleepchartview);
+        
+        sleepChartView.showWeekBarChart(null);
+        
+        
+        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				i++;
+				if (i%2==1) {
+					sleepChartView.showDayBarChart(null);
+				} else{
+					sleepChartView.showWeekBarChart(null);
+				}
+			}
+		});
 	}
 }
--- src/com/android/informationdemo/view/chart/SleepChartView.java
+++ src/com/android/informationdemo/view/chart/SleepChartView.java
@@ -6,13 +6,19 @@ import java.util.List;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
+import android.graphics.PathEffect;
+import android.graphics.Paint.FontMetrics;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+/**
+ * @author libill 2014-11-12
+ */
 public class SleepChartView extends View {
-	private String TAG="SleepChartView";
+	private String TAG = "SleepChartView";
 
 	private Context mContext;
 	private Paint mPaint = new Paint();
@@ -20,27 +26,28 @@ public class SleepChartView extends View {
 	private float height;
 
 	private int xSpaceNum = 20;
-	private int ySpaceNum = 3;
+	private int ySpaceNum = 4;
 	private float xSpace;
 	private float ySpace;
 
 	/** 四周边距 **/
-	float left, right, top, bottom;
+	private float left, right, top, bottom;
 
-	List<Float> xValues = new ArrayList<Float>();
-	List<Float> yValues = new ArrayList<Float>();
-	Float[] yFloats = { 1f, 2f, 3f };
+	private List<Float> xValues = new ArrayList<Float>();
+	private List<Float> yValues = new ArrayList<Float>();
+	private Float[] yFloats = { 1f, 2f, 3f };
+
+	private String[] weekXText = { "一", "二", "三", "四", "五", "六", "日" };
+
+	ChartType chartType = ChartType.DayBarChart;
+
+	private enum ChartType {
+		DayBarChart, WeekBarChart
+	}
 
 	public SleepChartView(Context context) {
 		super(context);
 		this.mContext = context;
-
-		for (int i = 0; i <= 6; i++) {
-			xValues.add(1f * i);
-		}
-		for (int i = 0; i <= 6; i++) {
-			yValues.add(1f * (1 + i % 3));
-		}
 	}
 
 	public SleepChartView(Context context, AttributeSet attrs) {
@@ -52,11 +59,14 @@ public class SleepChartView extends View {
 		super.onDraw(canvas);
 
 		initData();
+
 		drawBackground(canvas);
 
-		drawChart(canvas);
-
-		drawBarChart(canvas);
+		if (chartType == ChartType.DayBarChart) {
+			drawDayBarChart(canvas);
+		} else if (chartType == ChartType.WeekBarChart) {
+			drawWeekBarChart(canvas);
+		}
 
 	}
 
@@ -64,15 +74,14 @@ public class SleepChartView extends View {
 		width = this.getWidth();
 		height = this.getHeight();
 
-		left = right = 120;
-		top = bottom = 120;
+		left = right = 60;
+		top = bottom = 60;
 
 		width = width - left - right;
 		height = height - top - bottom;
 
 		xSpace = width / xSpaceNum;
 		ySpace = height / ySpaceNum;
-
 	}
 
 	public void drawBackground(Canvas canvas) {
@@ -81,9 +90,9 @@ public class SleepChartView extends View {
 		canvas.drawRect(0, 0, width + left + right, height + top + bottom, mPaint);
 	}
 
-	public void drawChart(Canvas canvas) {
+	public void drawDayGridChart(Canvas canvas) {
 		// 创建画笔
-		mPaint.setColor(Color.GRAY);// 设置红色
+		mPaint.setColor(Color.GRAY);
 
 		mPaint.setAntiAlias(true);// 设置画笔的锯齿效果。 true是去除
 
@@ -111,10 +120,16 @@ public class SleepChartView extends View {
 		}
 	}
 
-	public void drawBarChart(Canvas canvas) {
+	public void drawDayBarChart(Canvas canvas) {
+
+		// 网格，用于测试
+		drawDayGridChart(canvas);
+
 		mPaint.setColor(Color.RED);
 		mPaint.setStyle(Paint.Style.FILL);
 		float temp = 0;
+
+		float locationMark = 0; // 判断画虚线
 		for (int i = 0; i < xValues.size(); i++) {
 			if (yValues.get(i) <= yFloats[0]) {
 				mPaint.setColor(Color.RED);
@@ -124,14 +139,143 @@ public class SleepChartView extends View {
 				mPaint.setColor(Color.GREEN);
 			}
 			if (left + xSpace * (xValues.get(i) + temp) <= left + width) {
+				// 画柱状图
 				canvas.drawRect(left + xSpace * temp, height + top - (ySpace * yValues.get(i)), left + xSpace * (xValues.get(i) + temp), bottom + height, mPaint);
+
+				if (locationMark == 0 && yValues.get(i) > yFloats[0] && yValues.get(i) <= yFloats[1]) {
+					PathEffect effects = new DashPathEffect(new float[] { 8, 8, 8, 8 }, 8);
+					mPaint.setPathEffect(effects);
+					mPaint.setStrokeWidth(2);
+					mPaint.setColor(Color.GRAY);
+					// 画虚线
+					canvas.drawLine(left + xSpace * temp, right, top + xSpace * temp, height + bottom, mPaint);
+					mPaint.setStrokeWidth(1);
+					mPaint.setPathEffect(null);
+					locationMark = left + xSpace * temp;
+				}
 			} else {
+				// if (height + top - (ySpace * yValues.get(i)) <= left +
+				// width+10) {
 				canvas.drawRect(left + xSpace * temp, height + top - (ySpace * yValues.get(i)), left + width, bottom + height, mPaint);
+				// Log.i(TAG, "error:容错，超出范围了");
+				// }
 				Log.i(TAG, "error:容错，超出范围");
 			}
 
 			temp += xValues.get(i);
 		}
+
+		Paint textPaint = new Paint();
+		textPaint.setColor(Color.GRAY);
+		textPaint.setStyle(Paint.Style.FILL);
+		textPaint.setAntiAlias(true);
+		textPaint.setTextSize(30);
+		textPaint.setTextAlign(Paint.Align.LEFT);
+		canvas.drawText("00:09", locationMark + 10, top + 40, textPaint);
+
+		// 底部文字
+		textPaint.setTextAlign(Paint.Align.CENTER);
+		canvas.drawText("11:09", left, height + top + bottom / 2, textPaint);
+
+		canvas.drawText("09:00", left + width, height + top + bottom / 2, textPaint);
+	}
+
+	public void drawWeekGridChart(Canvas canvas) {
+		// 创建画笔
+		mPaint.setColor(Color.GRAY);
+
+		mPaint.setAntiAlias(true);// 设置画笔的锯齿效果。 true是去除
+
+		float startX = left;
+		float startY = top;
+		float stopX = width + right;
+		float stopY = bottom;
+
+		// X横坐标
+		for (int i = 0; i <= ySpaceNum; i++) {
+			canvas.drawLine(startX, startY, stopX, stopY, mPaint);
+			startY += ySpace;
+			stopY += ySpace;
+		}
+
+		// Y横坐标
+		startX = left;
+		startY = right;
+		stopX = top;
+		stopY = height + bottom;
+		for (int i = 0; i <= xSpaceNum; i++) {
+			canvas.drawLine(startX, startY, stopX, stopY, mPaint);
+			startX += xSpace;
+			stopX += xSpace;
+		}
+	}
+
+	public void drawWeekBarChart(Canvas canvas) {
+		// 网格，用于测试
+		drawWeekGridChart(canvas);
+
+		Paint textPaint = new Paint();
+		textPaint.setColor(Color.GRAY);
+		textPaint.setStyle(Paint.Style.FILL);
+		textPaint.setAntiAlias(true);
+		textPaint.setTextSize(30);
+		textPaint.setTextAlign(Paint.Align.LEFT);
+		// 底部文字
+		textPaint.setTextAlign(Paint.Align.CENTER);
+		FontMetrics fm = textPaint.getFontMetrics();
+		float textHeight = fm.descent - fm.ascent;
+
+		mPaint.setColor(Color.RED);
+		mPaint.setStyle(Paint.Style.FILL);
+		float temp = 0;
+		float spaceTemp = xSpace / 3;
+		for (int i = 0; i < xValues.size(); i++) {
+			if (yValues.get(i) <= yFloats[0]) {
+				mPaint.setColor(Color.RED);
+			} else if (yValues.get(i) <= yFloats[1]) {
+				mPaint.setColor(Color.BLUE);
+			} else if (yValues.get(i) <= yFloats[2]) {
+				mPaint.setColor(Color.GREEN);
+			}
+			// 画柱状图
+			canvas.drawRect(left + xSpace * temp + spaceTemp, height + top - (ySpace * yValues.get(i)), left + xSpace * (xValues.get(i) + temp) - spaceTemp, bottom + height, mPaint);
+
+			// 底部文字
+			canvas.drawText(weekXText[i], left + xSpace * (xValues.get(i) + temp) - xSpace / 2, height + top + bottom / 2 + textHeight / 3, textPaint);
+
+			temp += xValues.get(i);
+		}
+	}
+
+	public void showDayBarChart(ArrayList<Float> dataList) {
+		chartType = ChartType.DayBarChart;
+		xSpaceNum = 20;
+		ySpaceNum = 4;
+		xValues.clear();
+		yValues.clear();
+		for (int i = 0; i <= 6; i++) {
+			xValues.add(1f * i);
+			yValues.add(1f * (1 + i % 3));
+		}
+
+		invalidate();
+	}
+
+	public void showWeekBarChart(ArrayList<Float> dataList) {
+		chartType = ChartType.WeekBarChart;
+		xSpaceNum = 7;
+		ySpaceNum = 13;
+		xValues.clear();
+		yValues.clear();
+		for (int i = 0; i < xSpaceNum; i++) {
+			xValues.add(1f);
+		}
+
+		for (int i = 0; i < ySpaceNum; i++) {
+			yValues.add(1f * (1 + (i * 3) % ySpaceNum));
+		}
+
+		invalidate();
 	}
 
 }
